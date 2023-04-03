ea70aa57973fdfc6a15c5de90df1538820db404e
Lauryne <lauryne921@live.fr>
2023-04-03 17:30:49 +0200

    add joke component

--- country-list/src/app/app.module.ts
+++ country-list/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CountryComponent } from './country/country.component';
+import { JokeComponent } from './joke/joke.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountryComponent
+    CountryComponent,
+    JokeComponent
   ],
   imports: [
     BrowserModule,
